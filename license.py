851ccb332756e30787f67efc4f8d7389d02d1b35
royqbear <88905182+royqbear@users.noreply.github.com>
2022-09-29 13:48:31 +0300

    Update license.py

--- license.py
+++ license.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import tkinter
 from tkinter import *
 import tkinter.font as font
 from tkinter import ttk
@@ -9,31 +8,46 @@ from tkinter import messagebox
 from time import sleep
 from time import sleep
 import os
+import webbrowser
+
 
 class Window:
     def __init__(self):
         self.window = Tk()
-        self.companies = self.scrap()  # getting list of companies from scrap class
         self.window.eval('tk::PlaceWindow . center')
         self.background = '#edfbff'
         self.window.config(background=self.background)
-        self.welcome_label = Label(self.window).place(relx=0.5,rely=0.5,anchor='center')
-        self.frame = Frame(self.window,background=self.background)
-        self.frame2 = Frame(self.window,background=self.background)
+        self.welcome_label = Label(self.window).place(relx=0.5, rely=0.5, anchor='center')
+        self.frame = Frame(self.window, background=self.background)
+        self.frame2 = Frame(self.window, background=self.background)
         self.window.geometry("500x500")
         self.window.title("License MarketPlace")
+        #user\pass for website:
+        self.username = "******"
+        self.password = "******"
 
+        # creating webdriver for chrome
+        try:
+            self.options = webdriver.ChromeOptions()
+            self.options.add_argument('--headless')
+            self.driver = webdriver.Chrome(options=self.options)
+            self.session_id = self.driver.session_id
+        except Exception:
+            messagebox.showerror("Version update required",
+                                 'Please update Chronium\nfile needs to be placed where the EXE file is located.')
+            webbrowser.open('https://chromedriver.chromium.org/downloads')  # Go to example.com
+            self.window.destroy()
+        #defininf all gui variables
+        self.companies = self.scrap()  # getting list of companies from scrap class
         self.company_list = Listbox(self.frame, exportselection=False, height=len(self.companies))
         self.choices = ['Buy', 'Cancel']
         # button creation = lambda calling multiple functions
         self.btn = Button(self.frame2, text='Submit', command=self.button_methods)
-        # self.btn = Button(self.frame2, text='Submit', command=lambda: [self.list_pick(), self.get_user_input(), self.get_spinbox()])
-
         self.buy_cancel = ttk.Combobox(self.frame2, values=self.choices,width=20)
         self.buy_cancel.current(0)
         self.user_input = Entry(self.frame2,width=30)
         self.userIn_label = Label(self.frame2, text="Enter name for the license",background=self.background)
-        self.buy_cancel_label = Label(self.frame2, text="Doy you want to Buy or Cancel?",background=self.background)
+        self.buy_cancel_label = Label(self.frame2, text="Do you want to Buy or Cancel?",background=self.background)
         self.mainlabel = Label(self.frame, text="Choose company please",background=self.background)
         self.numeric = Spinbox(self.frame2, from_=1, to_=100, width=10)
 
@@ -41,63 +55,47 @@ class Window:
         self.user_input.grid(row=4, column=0, pady=5)
 
 
-#getting the list of licenses from the website, based on user name entered
+    #getting the list of licenses from the website, based on user name entered
     def scrap(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        driver = webdriver.Chrome(options=options)
-        session_id = driver.session_id
 
         try:
-            driver.get("https://shop.cms1.co.il/my-subscriptions/?viewId=718")
+            self.driver.get("https://shop.cms1.co.il/my-subscriptions/?viewId=718")
         except Exception:
             print("cannot load website, refreshing")
         else:
-            driver.get("https://shop.cms1.co.il/login/")
-        username = "*******"
-        password = "*******"
+            self.driver.get("https://shop.cms1.co.il/login/")
 
-        login_user = driver.find_element(By.NAME, "fld-login-username")
-        login_user.send_keys(username)
-        login_pass = driver.find_element(By.NAME, "fld-login-password")
-        login_pass.send_keys(password)
-        driver.find_element(By.NAME, "fld-login-submit").click()
-        active_subs = driver.find_element(By.CLASS_NAME, "greenbtn")
+        login_user = self.driver.find_element(By.NAME, "fld-login-username")
+        login_user.send_keys(self.username)
+        login_pass = self.driver.find_element(By.NAME, "fld-login-password")
+        login_pass.send_keys(self.password)
+        self.driver.find_element(By.NAME, "fld-login-submit").click()
+        active_subs = self.driver.find_element(By.CLASS_NAME, "greenbtn")
         active_subs.click()
-        table = driver.find_elements(By.XPATH, "//a[@class='align-middle']")
+        table = self.driver.find_elements(By.XPATH, "//a[@class='align-middle']")
         lis = []
         for i in table:
             lis.append(i.text)
-        # print(lis)
         return lis
+
+    def return_to_license_page(self):
+        self.driver.get("https://shop.cms1.co.il/my-subscriptions/?viewId=718")
 
 #automation code for buying license
     def buy_lic(self):
+        self.return_to_license_page()
         company_name_from_app = self.list_pick()
         name_for_new_lic = self.get_user_input()
         amount_to_buy = self.get_spinbox()
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        driver = webdriver.Chrome(options=options)
-        driver.get("https://shop.cms1.co.il/login/")
-        username = "********"
-        password = "********"
 
-        login_user = driver.find_element(By.NAME, "fld-login-username")
-        login_user.send_keys(username)
-        login_pass = driver.find_element(By.NAME, "fld-login-password")
-        login_pass.send_keys(password)
-        driver.find_element(By.NAME, "fld-login-submit").click()
-        active_subs = driver.find_element(By.CLASS_NAME, "greenbtn")
-        active_subs.click()
         try:
-            el = driver.find_element(By.XPATH,
+            el = self.driver.find_element(By.XPATH,
                                      f"//*[@id='iw-subscription-context']/table/tbody/tr[{company_name_from_app}]/td[12]/div/button")
             el.click()
         except Exception:
             print("trying other method")
         try:
-            el = driver.find_element(By.XPATH,
+            el = self.driver.find_element(By.XPATH,
                                      f"//*[@id='iw-subscription-context']/table/tbody/tr[{company_name_from_app}]/td[11]/div/button")
             el.click()
         except Exception:
@@ -105,67 +103,53 @@ class Window:
             # messagebox.showerror("Error", "Coding needed - input given to scrap is not in scope")
         sleep(3)
         try:
-            buy = driver.find_element(By.XPATH, f'//*[@id="iw-subscription-context"]/table/tbody/tr[{company_name_from_app}]/td[12]/div/div/button[1]')
+            buy = self.driver.find_element(By.XPATH, f'//*[@id="iw-subscription-context"]/table/tbody/tr[{company_name_from_app}]/td[12]/div/div/button[1]')
             buy.click()
         except Exception:
             print('tryin again')
 
         try:
-            buy = driver.find_element(By.XPATH, f'//*[@id="iw-subscription-context"]/table/tbody/tr[{company_name_from_app}]/td[11]/div/div/button[1]')
+            buy = self.driver.find_element(By.XPATH, f'//*[@id="iw-subscription-context"]/table/tbody/tr[{company_name_from_app}]/td[11]/div/div/button[1]')
             buy.click()
         except Exception:
             print("cannot find")
             messagebox.showerror("Error","I dont know what to do with myself...")
-        popup = driver.find_element(By.CLASS_NAME, "modal-content")
+        popup = self.driver.find_element(By.CLASS_NAME, "modal-content")
         sleep(3)
-        amount = driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div/div/div/div/div[2]/div/input')
+        amount = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div/div/div/div/div[2]/div/input')
         print(amount.text)
         amount.send_keys(Keys.CONTROL + "a")
         amount.send_keys(Keys.DELETE)
         amount.send_keys(amount_to_buy)
-        submit_buy = driver.find_element(By.XPATH, '//*[@id="buylicense-modal___BV_modal_footer_"]/div/button[2]')
+        submit_buy = self.driver.find_element(By.XPATH, '//*[@id="buylicense-modal___BV_modal_footer_"]/div/button[2]')
         submit_buy.click()
         sleep(3)
-        input_user_name_fromapp = driver.find_element(By.XPATH,
+        input_user_name_fromapp = self.driver.find_element(By.XPATH,
                                                       '/html/body/form/main/div[2]/div/div/section/div[3]/div/div/div[2]/div/div[3]/div[3]/fieldset/div[3]/div/div/input')
         input_user_name_fromapp.send_keys(name_for_new_lic)
-        approve_buy = driver.find_element(By.XPATH,
+        approve_buy = self.driver.find_element(By.XPATH,
                                           '//*[@id="checkout-step-1"]/div[3]/div/div/div[2]/div/div[4]/div[2]/button')
         approve_buy.click()
-        final_click = driver.find_element(By.XPATH,
+        final_click = self.driver.find_element(By.XPATH,
                                           '//*[@id="checkout-step-2"]/div[3]/div/div/div[2]/div/div[2]/div/div/div/button[2]')
         final_click.click()
+        messagebox.showinfo("Success","Operation completed")
         sleep(2)
+
 #automation code for cancelling a license
     def cancel_license_scrap(self):
+        self.return_to_license_page()
         company_name_from_app = self.list_pick()
         name_for_new_lic =  self.get_user_input()
         cancel_amount = self.get_spinbox()
         try:
-            options = webdriver.ChromeOptions()
-            options.add_argument('--headless')
-            driver = webdriver.Chrome(options=options)
-        except Exception:
-            messagebox.showerror("Version update required", 'Please update Chronium')
-        driver.get("https://shop.cms1.co.il/login/")
-        username = "*******"
-        password = "*******"
-
-        login_user = driver.find_element(By.NAME, "fld-login-username")
-        login_user.send_keys(username)
-        login_pass = driver.find_element(By.NAME, "fld-login-password")
-        login_pass.send_keys(password)
-        driver.find_element(By.NAME, "fld-login-submit").click()
-        active_subs = driver.find_element(By.CLASS_NAME, "greenbtn")
-        active_subs.click()
-        try:
-            company_row = driver.find_element(By.XPATH,
+            company_row = self.driver.find_element(By.XPATH,
                                               f"//*[@id='iw-subscription-context']/table/tbody/tr[{company_name_from_app}]/td[12]/div/button")
             company_row.click()
         except Exception:
             print("trying other method")
         try:
-            company_row = driver.find_element(By.XPATH,
+            company_row = self.driver.find_element(By.XPATH,
                                               f"//*[@id='iw-subscription-context']/table/tbody/tr[{company_name_from_app}]/td[11]/div/button")
             company_row.click()
         except Exception:
@@ -173,27 +157,28 @@ class Window:
             # messagebox.showerror("Error", "Coding needed - input given to scrap is not in scope")
 
         try:
-            cancel = driver.find_element(By.XPATH,
+            cancel = self.driver.find_element(By.XPATH,
                                          f'//*[@id="iw-subscription-context"]/table/tbody/tr[{company_name_from_app}]/td[11]/div/div/button[3]')
             cancel.click()
         except Exception:
             pass
         try:
-            cancel = driver.find_element(By.XPATH,
+            cancel = self.driver.find_element(By.XPATH,
                                          f'//*[@id="iw-subscription-context"]/table/tbody/tr[{company_name_from_app}]/td[12]/div/div/button[3]')
             cancel.click()
         except Exception:
             print('code error')
         sleep(3)
-        amount_to_cancel = driver.find_element(By.XPATH, '//*[@id="__BVID__29"]')
+        amount_to_cancel = self.driver.find_element(By.XPATH, '//*[@id="__BVID__29"]')
         amount_to_cancel.send_keys(Keys.CONTROL + "a")
         amount_to_cancel.send_keys(Keys.DELETE)
         amount_to_cancel.send_keys(cancel_amount)
-        cancel_button = driver.find_element(By.XPATH,
+        cancel_button = self.driver.find_element(By.XPATH,
                                             '//*[@id="cancel-subscription-modal___BV_modal_footer_"]/div/div/button[2]')
         cancel_button.click()
-        messagebox.showinfo("done", "DONE")
-        sleep(5)
+        messagebox.showinfo("Success", "Operation Completed")
+        sleep(2)
+
 #creating the app
     def app(self):
         # creating a frame for the lists
@@ -266,7 +251,7 @@ class Window:
         print(number_of_lics)
         return number_of_lics
 
-#like driver code for the submit button.
+#like self.driver code for the submit button.
     def button_methods(self):
         if not self.list_pick():
             pass
@@ -288,7 +273,6 @@ class Login:
         self.tkWindow.geometry('200x150')
         self.tkWindow.title('Login')
         self.tkWindow.eval('tk::PlaceWindow . center')
-        self.tkWindow.iconbitmap(r'C:\Users\royqb\Desktop\python_learning\office365\img\icon.ico')
         background = '#edfbff'
         self.tkWindow.configure(background=background)
         myfont = font.Font(family="segoe print", size=10)
@@ -322,12 +306,10 @@ class Login:
             self.load()
 
 if __name__ == "__main__":
-    useros = os.environ.get('cmsloginuser')
-    userpassos = os.environ.get("cmsloginpass")
+    useros = os.environ.get('youruserid')
+    userpassos = os.environ.get("yourpass")
     login = Login(useros,userpassos)
     login.load()
-    # if login.validateLogin():
-    #     w = Window()
-    #     w.app()
+
 
 
